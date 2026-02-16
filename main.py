b64c22ae9d8cbaceed99af4456a88fc60add55b8
izichka <zetagames120@gmail.com>
2026-02-16 11:12:33 +0300

    start calss

---
+++ main.py
@@ -0,0 +1,18 @@
+from PyQt5 import QtWidgets, QtCore, QtGui
+from ui import Ui_MainWindow
+import sys
+
+class Main(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = Main()
+    window.show()
+    window.raise_()
+    sys.exit(app.exec())
